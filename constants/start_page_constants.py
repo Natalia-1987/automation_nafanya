6832dd9f25953b99989e54b14ff3d5b43aa89d4e
nataliamurzahanova <ndomanska@gmail.com>
2023-08-29 10:36:11 +0300

    Initial commit

---
+++ constants/start_page_constants.py
@@ -0,0 +1,24 @@
+class StartPageConstants:
+    # URL = "https://www.slotozilla.com/"
+    # GG_BET_GET_BONUS_XPATH = ".//div[@data-pid='ggbet-casino-wb-en']"
+    # HEADER_SEARCH_BUTTON_XPATH = ".//div[@id='header-search-hide']"
+    # LOGIN_value_ADMIN = "QWERTY12345"
+    URL = "https://automatenspielex.com/"
+
+    HEADING_H1_XPATH = ".//h1"
+    HEADING_H2_XPATH = ".//h2[@id='Spielen_Sie_mit_unseren_Spielautomaten']"
+    HEADER_LOGO_XPATH = ".//div[@class='header__logo']"
+    FOOTER_BZGA_XPATH = ".//img[@title='BZgA']"
+    SYNDICATE_CASINO_SPIELEN_XPATH = ".//span[@id='top-3-btn-1']"
+    HEADER_SEARCH_BUTTON_XPATH = ".//div[@class='search-btn']"
+    SORT_SEARCH_BUTTON_XPATH = ".//input[@id='slots-search-input']"
+    AM_BELIEBTESTEN_SORT_XPATH = ".//*/div[text()='Am Beliebtesten']"
+    ALLE_SPIELE_FILTER_XPATH = ".//*/div[text()='Alle Spiele']"
+    ALLE_ANBIETER_FILTER_XPATH = ".//*/div[text()='Alle Anbieter']"
+    FILTER_RESET_XPATH = ".//button[@id='slots-reset-btn']"
+    MEGA_JOKER_QUICK_LINK = ".//*/span[text()='Mega Joker']"
+    EMAIL_FOOTER_XPATH = ".//a[@class='footer_email']"
+    COUNT_SPIELE_XPATH = ".//div[@id='slots-count']"
+    NEXT_PAGE_PAGINATION_XPATH = ".//span[@class='next page-numbers']"
+
+
